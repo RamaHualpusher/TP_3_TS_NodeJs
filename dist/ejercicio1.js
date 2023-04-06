1a34170507e87634254a0f54515692bd371cc3ce
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-04-06 02:39:57 -0300

    Se limpia el proyecto en GIT

--- dist/ejercicio1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resultadoDoble = void 0;
-// Convertir la función de JS a una función de flecha de TS
-const resultadoDoble = (a, b) => (a + b) * 2;
-exports.resultadoDoble = resultadoDoble;
-//# sourceMappingURL=ejercicio1.js.map
